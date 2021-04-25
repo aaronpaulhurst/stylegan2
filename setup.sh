8c75b286012de38d4aec4b31fa2605f5caad4204
aaronpaulhurst <aaronpaulhurst@gmail.com>
2021-04-25 13:38:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@
 #   git clone https://github.com/aaronpaulhurst/stylegan2.git
 
 # Install ubuntu packages
-sudo apt update 
-sudo apt install software-properties-common gcc g++ make python3-venv nvidia-driver-460 awscli
+sudo apt-get update 
+sudo apt-get --assume-yes install software-properties-common gcc g++ make python3-venv nvidia-driver-460 awscli
 
 # Create and activate python virtual environment
 rm -rf ~/venv
@@ -19,13 +19,8 @@ pip install --upgrade pip
 pip install --upgrade tensorflow-gpu==1.14
 pip install Pillow
 
-# Install CUDA 10 (from NVIDIA site)
-wget https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda_10.0.130_410.48_linux
-chmod a+x cuda_10.0.130_410.48_linux
-sudo ./cuda_10.0.130_410.48_linux
-
 # Install CNN (from local S3 bucket)
-aws configure
+#aws configure
 aws s3 cp s3://ahurst-stylegan2/cuda_10.0.130_410.48_linux.run .
 aws s3 cp s3://ahurst-stylegan2/libcudnn7_7.5.0.56-1+cuda10.0_amd64.deb .
 aws s3 cp s3://ahurst-stylegan2/libcudnn7-dev_7.5.0.56-1+cuda10.0_amd64.deb .
